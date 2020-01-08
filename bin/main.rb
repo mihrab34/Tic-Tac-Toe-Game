dea6aa8db76cb1e756c118bd65dd4ff77492a6d6
Rahmat <akintolarahmah@gmail.com>
2020-01-08 13:55:29 +0000

    initial commit

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,26 @@
 #!/usr/bin/env ruby
 
-puts "Hello World!"
+puts "Welcome to Tic Tac Toe!"
+puts
+
+def display_board
+
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+end
+
+puts "What move would X like to make (1-9)?"
+puts
+
+board = []
+def player_input(input)
+
+    player = gets.chomp.to_i
+end
+
+def player_move(array, index, value = "X")
+
+end
