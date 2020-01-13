7ad0ddb4d3a762135e7ec9b8bfba1a292546593d
Samuel <babssam69@gmail.com>
2020-01-13 22:20:00 +0000

    Added main.rb file under the game_logic branch

--- lib/game.rb
+++ lib/game.rb
@@ -26,11 +26,25 @@ class Game
     public
 
     def invalid_position(position)
+        return "Position occupied with #{@board[position - 1]}" unless @board[position - 1].nil?
+        return 'Position out of boundaries. Please numbers from 1 to 9' unless position >= 1 && position <= 9
+
+    false
 
     end
 
     def move_game 
+        @board[position - 1] = current_turn
+        @turn_x = !@turn_x
+        @total_turns -= 1
+        winner = winner(position - 1)
 
+    return "It's a draw" if !winner && total_turns < 1
+
+    return winner unless winner
+
+        @total_turns = 0 if winner
+    return "The champion is #{@player[winner.downcase.to_sym].name} '#{winner}'!!" if winner
     end
 
     def current_turn
@@ -38,9 +52,21 @@ class Game
     end
 
     def show_board
-
+        numbers_with_vertical_sides = [1, 4, 7]
+        board = ''
+        9.times do |x|
+        board += numbers_with_vertical_sides.include?(x) ? '|' : ''
+        board += if @total_turns == 9
+                 " #{x + 1} "
+                else
+                 " #{@board[x] || ' '} "
+               end
+        board += numbers_with_vertical_sides.include?(x) ? '|' : ''
+        board += numbers_with_vertical_sides.include?(x + 2) ? "\n---|---|---\n" : ''
     end
-        
+    board
+    end
+
     def reset_game
         @turn_x = true
         @board = [nil] * 9
